e86dad6030b9301889e729009606c03ef2bf111c
Daysha Jones <daysha.vu@icloud.com>
2021-03-17 18:20:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,12 +7,12 @@ class EmailAddressParser
   
   attr_accessor :email
   
-  def initialize
-    
+  def initialize(email)
+    @email = email
   end
   
   def parse
-    
+    @email.split(/[,\s+/).uniq
   end
   
 end
